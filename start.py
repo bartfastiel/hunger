d0d44ccba306c7de8f0da950bce9fdbc2a9fb9fc
bartfastiel <bartfastiel@users.noreply.github.com>
2019-02-17 16:11:54 +0100

    make plot use seconds diff to first entry

--- start.py
+++ start.py
@@ -39,8 +39,9 @@ def process_diary(ingredients):
     wellbeing_per_datetime = {}
     diary_title_pattern = re.compile("^(\\d+)\\.(\\d+)\\.(\\d+) (\\d+):(\\d+) ((\\d+)(.(\\d+))?)$")
     next_day = 1
-    diary_datetime = 0
+    first_datetime = 0
     for line in diary_raw:
+        diary_offset = 0
         line = line.rstrip()
         if not line:
             next_day = 1
@@ -59,19 +60,24 @@ def process_diary(ingredients):
 
                 diary_datetime = datetime(int(year), int(month), int(day), int(hour), int(minute))
 
-                wellbeing_per_datetime[diary_datetime] = float(m.group(6))
+                if not first_datetime:
+                    first_datetime = diary_datetime
+
+                diary_offset = (diary_datetime - first_datetime).total_seconds()
+                wellbeing_per_datetime[diary_offset] = float(m.group(6))
             else:
                 contents = []
-                if diary_datetime in ingredients_per_datetime:
-                    contents = ingredients_per_datetime[diary_datetime]
+                if diary_offset in ingredients_per_datetime:
+                    contents = ingredients_per_datetime[diary_offset]
                 else:
-                    ingredients_per_datetime[diary_datetime] = contents
+                    ingredients_per_datetime[diary_offset] = contents
                 contents.append(line)
                 if line in ingredients:
                     for synonym in ingredients[line]:
                         contents.append(synonym)
-                ingredients_per_datetime[diary_datetime] = contents
+                ingredients_per_datetime[diary_offset] = contents
             next_day = 0
+    pprint(first_datetime)
     return ingredients_per_datetime, wellbeing_per_datetime
 
 
@@ -92,5 +98,8 @@ print()
 
 x = wellbeing_diary.keys()
 y = wellbeing_diary.values()
-plt.plot_date(x, y, linestyle='solid')
+x2 = []
+x2.extend(x)
+
+plt.plot(x, y, linestyle='solid')
 plt.show()
