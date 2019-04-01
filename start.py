535f1c478f35ad611ef384618cf50df1170fe757
bartfastiel <bartfastiel@users.noreply.github.com>
2019-04-01 20:25:14 +0200

    highlight areas of positive/negative effect

--- start.py
+++ start.py
@@ -5,6 +5,9 @@ import io
 from datetime import datetime
 import matplotlib.pyplot as plt
 
+lower_limit = -1 * 24
+upper_limit = +4 * 24
+
 sort_by_number_of_consumptions = False
 
 def load_ingredients(raw):
@@ -151,6 +154,7 @@ fig.set_size_inches(10, number_of_ingredients * 2)
 plot_number = 0
 
 for inspected in interesting_ingredients:
+    print(inspected)
     consumption_times = []
 
     for consumption_time in ingredients_diary.keys():
@@ -163,22 +167,33 @@ for inspected in interesting_ingredients:
         subplot = axs
 
     well_being_per_relative_time = []
-    for i in range(-1 * 24, +4 * 24):
+    x = range(lower_limit, upper_limit)
+    for i in x:
         well_being_per_relative_time.append([])
 
     for consumption_time in consumption_times:
-        for relative_time in range(-1 * 24, +4 * 24):
+        for relative_time in x:
             if 0 < (consumption_time + relative_time) < len(well_beings):
                 well_being_per_relative_time[relative_time + 24].append(well_beings[consumption_time + relative_time])
         subplot.plot([time - consumption_time for time in times], well_beings, linestyle='-', color='#aaaaaa')
 
     average_well_being = [mean(x) for x in well_being_per_relative_time]
-    subplot.plot(range(-1 * 24, +4 * 24), average_well_being, linestyle='-', )
+    subplot.plot(x, average_well_being, linestyle='-', )
+
+    if not any([(not x) for x in average_well_being]):
+        average_well_being_at_consumption = average_well_being[24]
+        positive_effect = [y >= average_well_being_at_consumption for y in average_well_being]
+        negative_effect = [y <= average_well_being_at_consumption for y in average_well_being]
+        subplot.fill_between(x, average_well_being, average_well_being_at_consumption, where=positive_effect, facecolor='green', interpolate=True)
+        subplot.fill_between(x, average_well_being, average_well_being_at_consumption, where=negative_effect, facecolor='red', interpolate=True)
 
     subplot.set_title(inspected)
-    subplot.set_xlim([-1 * 24, +4 * 24])
-    subplot.set_xticks([0 + x * 24 for x in range(-1, +5)])
-    subplot.set_xticklabels(["day " + str(x) for x in range(-1, +5)])
+    subplot.set_xlim([lower_limit, upper_limit])
+    upper_days = int(upper_limit / 24)
+    lower_days = int(lower_limit / 24)
+    days = range(lower_days, upper_days)
+    subplot.set_xticks([0 + x * 24 for x in days])
+    subplot.set_xticklabels(["day " + str(x) for x in days])
 
     plot_number += 1
 
